f6fc6ef38d1d6b7bd778b6bbb76c61bbac063fbe
LockDown-Ammo <76776824+LockDown-Ammo@users.noreply.github.com>
2022-08-03 22:37:37 +0530

    Fixed #5, Resolved #6

--- routers/docs.ts
+++ routers/docs.ts
@@ -1,8 +1,8 @@
 import fs from 'fs'
 
 export default async (app: any) => {
-  app.get('/docsraw', (req: any, res: any) => {
-    const data = fs.readFileSync('../README.md')
+  app.get('/docs', (req: any, res: any) => {
+    const data = fs.readFileSync('./README.md')
     res.send(data.toString()).catch((e: any) => console.log(e));
   })
 }
