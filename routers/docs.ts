b68221f06e9d38ae46252d5d4ec8df6b66216354
Hey there! I am using WhatsApp <76776824+LockDown-Ammo@users.noreply.github.com>
2022-09-09 05:05:03 +0000

    Updated markdown rendering for docs

--- routers/docs.ts
+++ routers/docs.ts
@@ -1,27 +1,28 @@
 import fs from 'fs'
 import md from 'markdown-it'
 const marked = md({
-    html: true,
-    linkify: true,
-    typographer: true,
-    langPrefix: '',
+  html: true,
+  linkify: true,
+  typographer: true,
+  langPrefix: '',
 });
 export default async (app: any) => {
 
-    const githubMarkdownCSS = await import('generate-github-markdown-css')
-    app.get('/docs', async (req: any, res: any) => {
-        try {
-            const data = fs.readFileSync('./README.md')
-            const r = marked.render(data.toString())
-            const htmlData =
-                `<style>
+  const githubMarkdownCSS = await import('generate-github-markdown-css')
+  app.get('/docs', async (req: any, res: any) => {
+    res.set({
+      'Access-Control-Allow-Origin': '*'
+    })
+    try {
+      const data = fs.readFileSync('./README.md')
+      const r = marked.render(data.toString())
+      const htmlData =
+        `<style>
                  body {
                    background-color: #0d1117
                 }
                 .markdown-body {
                     box-sizing: border-box;
-                    min-width: 200px;
-                    max-width: 980px;
                     margin: 0 auto;
                     padding: 45px;
                 }
@@ -31,15 +32,26 @@ export default async (app: any) => {
                         padding: 15px;
                     }
                 }
-                
-                ${await githubMarkdownCSS.default({ dark: 'dark' })}
+                ${fs.readFileSync('./githubMarkDown.css').toString()}
             </style>
-            <div class="markdown-body">
+            <link rel="stylesheet" href="githubMarkDown.css">
+            <link rel="stylesheet" href="https://raw.githubusercontent.com/sindresorhus/github-markdown-css/main/github-markdown.css">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.3/gh-fork-ribbon.min.css">
+		<style>
+			.github-fork-ribbon:before {
+				background-color: #121612;
+			}
+		</style>
+            <article class="markdown-body">
                 ${r}
-            </div>`
-            res.send(htmlData)
-        } catch (e) {
-            console.log(e)
-        }
-    })
+            </article>`
+      res.send(htmlData)
+    } catch (e) {
+      console.log(e)
+      res.send(
+        'Sorry an internal error occured and CSS couldnt be loaded... ( or maybe something worse )' +
+        marked.render(fs.readFileSync('./README.md').toString())
+      )
+    }
+  })
 }
